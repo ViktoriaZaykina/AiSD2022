1c06787bd88576e250735529ad01aae7fd7bcbb8
ViktoriaZaykina <xx.toi14@mail.ru>
2024-04-20 20:15:39 +0500

    Начальное окно, кнопка старт.
    Добавлено начальное окно и кнопка старт.

---
+++ WpfApp1/WpfApp1/NewFolder1/CodeFile1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AB
+{
+    public class Program
+    {
+        public class Bird
+        {
+            List<double> Coord1 = new List<double>();
+            List<double> Coord2 = new List<double>();
+
+            public void Fly()
+            {
+                const double g = 9.81, pi = Math.PI;
+                double alpha, v, t, T, deltaT = 0.1;
+                using (StreamReader read = new StreamReader("дано.txt"))
+                {
+                    v = Convert.ToDouble(read.ReadLine());
+                    alpha = Convert.ToDouble(read.ReadLine());
+                    alpha = alpha * pi / 180;
+                    T = 2 * v * Math.Sin(alpha) / g;
+                }
+
+                double x = 0;
+                double y = 0;
+                double Vx = v * Math.Cos(alpha);
+                double Vy = v * Math.Sin(alpha);
+
+                for (t = 0; t <= T; t += deltaT)
+                {
+                    x = x + Vx * deltaT;
+                    y = y + Vy * deltaT;
+                    Vy = Vy - g * deltaT;
+                    Coord1.Add(x);
+                    Coord2.Add(y);
+                }
+
+                using (StreamWriter write = new StreamWriter("итог.txt"))
+                {
+                    for (int n = 0; n < Coord1.Count; n++)
+                    {
+                        write.WriteLine($"x = {Coord1[n]}");
+                    }
+                    for (int m = 0; m < Coord2.Count; m++)
+                    {
+                        write.WriteLine($"y = {Coord2[m]}");
+                    }
+                }
+            }
+
+            public static void Udar()
+            {
+                Console.WriteLine("Произошло столкновение!");
+            }
+
+            public delegate void UdarDelegate();
+
+            public event UdarDelegate Stolk;
+
+            public void Proverka()
+            {
+                for (int i = 0; i < Coord1.Count; i++)
+                {
+                    if (Coord1[i] >= 7)
+                    {
+                        Stolk();
+                        return;
+                    }
+                }
+            }
+
+        }
+
+        public static void Main()
+        {
+            Bird redB = new Bird();
+            redB.Stolk += Bird.Udar;
+            redB.Fly();
+            redB.Proverka();
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/NewFolder1/Window.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Documents;
+using System.Windows.Media.Imaging;
+
+namespace AB
+{
+    class ABWindow : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new ABWindow());
+        }
+        public ABWindow()
+        {
+            Title = "Angry Birds";
+
+            Uri uri = new Uri("https://wp-top.ru/wp-content/uploads/2023/04/genshin-impakt-1091576.jpg");
+            BitmapImage bitmap = new BitmapImage(uri);
+            Image img = new Image();
+            img.Source = bitmap;
+            img.Stretch = Stretch.UniformToFill;
+            Content = img;
+
+            Button btn = new Button();
+            btn.Content = "Старт!";
+            btn.Click += ButtonOnClick;
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.VerticalAlignment = VerticalAlignment.Center;
+            Content = btn;
+        }
+        void ButtonOnClick(object sender, RoutedEventArgs args)
+        {
+            MessageBox.Show("The button has been clicked and all is well.", Title);
+        }
+
+    }
+}
