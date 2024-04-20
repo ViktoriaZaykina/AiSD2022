aba5c02ccbc565ccf809c37998aeae3277802b7d
ViktoriaZaykina <xx.toi14@mail.ru>
2024-04-20 20:09:34 +0500

    Прототип Angry Birds
    Добавлено начальное окно и кнопка старт.

---
+++ C# progects/CodeFile1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 8, 2, 6 };
+
+            int y = 0;
+
+            foreach (int x in arr)
+            {
+
+                y += x / 2;
+
+            }
+
+            Console.Write(y);
+        }
+    }
+}
